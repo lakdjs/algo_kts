783ca6753f58484cf5906c59f603ed35c206e913
praprf <fil18mot@gmail.com>
2023-12-24 18:46:22 +0300

    added project

---
+++ Graph_Kt/my_collections/__init__.py
@@ -0,0 +1,10 @@
+#__init__.py
+from .set import MySet
+# __init__.py
+from .set import MySet
+from .queue import Queue
+
+# __init__.py
+from .set import MySet
+from .queue import Queue
+from.heap import Heap, IKey
